0b1e266c997f66b127b6a3dfc33acecf02ce025d
jenkinsairwdemo <jenkins@airwdemo.com>
2015-04-06 14:56:17 -0400

    Initial Commit
    Commit

---
+++ JenkinsCIDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenkinsCIDemo
+//
+//  Created by Shardul Navare on 4/6/15.
+//  Copyright (c) 2015 AirWatch Demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JenkinsCIDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsCIDemo
+//
+//  Created by Shardul Navare on 4/6/15.
+//  Copyright (c) 2015 AirWatch Demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
